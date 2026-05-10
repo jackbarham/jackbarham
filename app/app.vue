50f158d06d759c704c065ee610bbbd15101c729d
Jack Barham <>
2026-05-10 12:19:15 +0100

    Initial setup

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Jack Barham</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxtjs/sanity',
+    'nuxt-svgo',
+    'vue3-carousel-nuxt'
+  ]
+})
